0cbfe707088ef3f23103524152bb9f1bb77b877a
JosephRidge <josephridge18@gmail.com>
2022-09-24 16:25:26 +0300

    implementing PUT method

--- demo/src/main/java/demo/demo/student/StudentController.java
+++ demo/src/main/java/demo/demo/student/StudentController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,8 +41,12 @@ import org.springframework.web.bind.annotation.RestController;
          studentService.deleteStudent(studentId);
      }
 
-     @Transactional
-     public void updateStudent(@RequestBody Student student){
-         studentService.updateStudent(student);
+     @PutMapping(path ="{studentId}")
+     public void updateStudent(
+         @PathVariable("studentId") Long studentId, 
+         @RequestParam(required = false) String name, 
+         @RequestParam(required = false) String email
+         ){
+         studentService.updateStudent(studentId, name, email);
      }
 }
--- demo/src/main/java/demo/demo/student/StudentService.java
+++ demo/src/main/java/demo/demo/student/StudentService.java
@@ -1,10 +1,12 @@
 package demo.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service // makes it be a bean
 public class StudentService {
@@ -27,7 +29,7 @@ public class StudentService {
             throw new IllegalStateException("email taken");
         }
 
-        studentRepository.save(student); 
+        studentRepository.save(student);
     }
 
     public void deleteStudent(Long studentId) {
@@ -39,7 +41,21 @@ public class StudentService {
         studentRepository.deleteById(studentId);
     }
 
-    public void updateStudent(Student student) {
-            studentRepository.save(student);
+    @Transactional
+    public void updateStudent(Long studentId,
+            String name,
+            String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exist"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+        if (email != null && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken !");
+            }
+            student.setEmail(email);
+        }
     }
 }
